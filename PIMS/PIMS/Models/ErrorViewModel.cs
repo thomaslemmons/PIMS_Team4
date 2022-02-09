3122b3c7402625ed17aaf8314ff7f060e0e2b7ea
thomaslemmons <73408475+thomaslemmons@users.noreply.github.com>
2022-02-08 19:26:02 -0600

    Insert blank project

---
+++ PIMS/PIMS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PIMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
