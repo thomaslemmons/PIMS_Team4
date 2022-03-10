9d9c7ed9fec6bc4fdbd81d7224be4c5cd19328de
thomaslemmons <73408475+thomaslemmons@users.noreply.github.com>
2022-03-10 16:46:46 -0600

    Put in rough template of program

--- PIMS/PIMS/Controllers/HomeController.cs
+++ PIMS/PIMS/Controllers/HomeController.cs
@@ -9,6 +9,14 @@ using System.Threading.Tasks;
 
 namespace PIMS.Controllers
 {
+    enum Role
+    {
+        Doctor,
+        MedicalStaff,
+        OfficeStaff,
+        Volunteer
+    }
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -21,6 +29,92 @@ namespace PIMS.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult DoctorPatientList()
+        {
+            //get list of all patients from db
+
+            List<Patient> PList = GetPatientList();
+
+            return View();
+        }
+
+        public IActionResult DoctorPatientDetail(int id)
+        {
+            //get info for singular patient
+
+            Patient p = GetPatient(id);
+
+            return View();
+        }
+
+        public IActionResult MedicalPatientList()
+        {
+            //get list of all patients from db
+
+            List<Patient> PList = GetPatientList();
+
+            return View();
+        }
+
+        public IActionResult MedicalPatientDetail(int id)
+        {
+            //get info for singular patient
+
+            Patient p = GetPatient(id);
+
+            return View();
+        }
+
+        public IActionResult OfficePatientList()
+        {
+            //get list of all patients from db
+
+            List<Patient> PList = GetPatientList();
+
+            return View();
+        }
+
+        public IActionResult OfficePatientDetail(int id)
+        {
+            //get info for singular patient
+
+            Patient p = GetPatient(id);
+
+            return View();
+        }
+
+        public IActionResult VolunteerPatientList()
+        {
+            //get list of all patients from db
+
+            List<Patient> PList = GetPatientList();
+
+            return View();
+        }
+
+        public IActionResult VolunteerPatientDetail(int id)
+        {
+            //get info for singular patient
+
+            Patient p = GetPatient(id);
+
+            return View();
+        }
+
+        public List<Patient> GetPatientList()
+        {
+            //call database and get list of patients
+            List<Patient> PList = new List<Patient>();
+            return PList;
+        }
+
+        public Patient GetPatient(int id)
+        {
+            //get all information about individual patient
+            Patient p = new Patient();
+            return p;
         }
 
         public IActionResult Privacy()
---
+++ PIMS/PIMS/Models/HomeModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIMS.Models
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string HomePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string EmergencyContact1 { get; set; }
+        public string EmergencyContact2 { get; set; }
+        public string AdmitDate { get; set; }
+        public string AdmitTime { get; set; }
+        public string AdmitReason { get; set; }
+        public string FamilyDoctor { get; set; }
+        public string Facility { get; set; }
+        public string Floor { get; set; }
+        public string RoomNumber { get; set; }
+        public string BedNumber { get; set; }
+        public string DischargeDate { get; set; }
+        public string DischargeTime { get; set; }
+        public string DoctorNotes { get; set; }
+        public string NurseNotes { get; set; }
+        public string Prescriptions { get; set; }
+        public string Procedures { get; set; }
+        public string InsuranceCarrier { get; set; }
+        public string InsuranceInfo { get; set; }
+        public string BillingInfo { get; set; }
+        public string AmountPaid { get; set; }
+        public string AmountOwed { get; set; }
+        public string AmountInsurancePaid { get; set; }
+        public string ApprovedVisitors { get; set; }
+        public string NumberOfVisitors { get; set; }
+    }
+
+    public class PatientList
+    {
+        public List<Patient> PList { get; set; }
+        public int role { get; set; }
+    }
+}
