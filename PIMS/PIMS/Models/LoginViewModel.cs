c800dd0a5e46a9c57adcbc4bb8031ce7cff23acf
kelseygrant13 <62297841+kelseygrant13@users.noreply.github.com>
2022-04-20 18:23:16 -0500

    Add files via upload

---
+++ PIMS/PIMS/Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LoginRegister.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name ="Email Address")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ PIMS/PIMS/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LoginRegister.Models
+{
+    public class RegisterViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
