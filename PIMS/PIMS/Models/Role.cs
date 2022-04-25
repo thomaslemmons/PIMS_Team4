b9cd6bdf79b90c40a7a5dc168179af986d7399e9
kelseygrant13 <62297841+kelseygrant13@users.noreply.github.com>
2022-04-25 18:12:32 -0500

    Create Role.cs

---
+++ PIMS/PIMS/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PIMS.Models
+{
+    public class RoleMod
+    {
+        [Key]
+        public int ID { get; set; } 
+        public string Email { get; set; }
+        public int Role { get; set; }
+    }
+}
